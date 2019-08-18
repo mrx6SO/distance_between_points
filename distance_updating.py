1832b49455e9a9c142818a3e3c101b25305ea738
mrx6SO <53888062+mrx6SO@users.noreply.github.com>
2019-08-18 09:43:05 -0300

    Create distance_updating.py

---
+++ distance_updating.py
@@ -0,0 +1,26 @@
+#!/usr/bin python 
+
+# -*- coding: utf-8 -*- 
+
+import math
+
+# Função para calcular distância entre dois pontos em um espaço Euclidiano bi-dimensional.
+
+def distance_betw_point():
+
+        result = math.sqrt(x-y)**2 + (x_-y_)**2
+
+        result_ = math.sqrt(result) 
+            
+        print result_
+
+if __name__ == "__main__":
+    
+    x=input() # 1 
+    y=input() # 1 
+
+    x_=input() # 4 
+    y_=input() # 1 
+     
+    distance_betw_point()
+
