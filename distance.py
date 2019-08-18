466a3be4447edaf3e65fac954b63f188cd24fb59
mrx6SO <53888062+mrx6SO@users.noreply.github.com>
2019-08-18 07:40:20 -0300

    Update distance.py

--- distance.py
+++ distance.py
@@ -4,11 +4,18 @@
 
 import math
 
-x=input()
-y=input()
-x_=input()
-y_=input()
-    
-result = math.sqrt(x-y)**2 + (x_-y_)**2
+x=input() # 1 
+y=input() # 1 
 
-print result 
+x_=input() # 4 
+y_=input() # 1 
+
+# Fórmula usada para estabelecer entre dois pontos distintos. (x1,x2) e (y1,y2)
+
+# dAB = \/(x1-x2)^2 + (y1 - y2) ^2 
+
+result = (math.sqrt(x-y)**2 + (x_-y_)**2)
+
+result_ = math.sqrt(result) 
+
+print result_ # 3 
