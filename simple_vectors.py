baed3ffc2cba4ffb6740cd34da0a06fc9fc5d8d1
mrx6SO <53888062+mrx6SO@users.noreply.github.com>
2019-08-19 08:14:57 -0300

    Update simple_vectors.py

--- simple_vectors.py
+++ simple_vectors.py
@@ -9,39 +9,54 @@ def check_vector():
        v = B - A 
     '''
 
-    vec = b-a
+    vec = b - a
 
-    print vec 
+    print vec
 
 def vector_module():
 
     ''' 
        v = (a,b) 
 
-    ''' 
+    '''
 
-    module_vec = math.sqrt(a**2+b**2) 
+    module_vec = math.sqrt(a**2+b**2)
 
-    print module_vec 
-
+    print module_vec
 
     pass
 
 
 def vector_uni():
 
-    vector_u = (b-a)/(math.sqrt(a**2+b**2)) 
+    vector_u = (b-a)/(math.sqrt(a**2+b**2))
 
-    
-    print vector_u 
+
+    print vector_u
+
+
+def build_basic_vec():
+
+    '''
+      v = (1,0) 
+    '''
+
+    print "v =", (c,d)
 
 if __name__ == "__main__":
 
     a=input("value of segment A: ")
     b=input("value of segment B: ")
 
+    c=input()
+    d=input()
+
     check_vector()
 
     vector_module()
- 
+
     vector_uni()
+
+    build_vec()
+    
+# TODO: scalar product.     
